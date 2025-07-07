13eb43d45e7cab5e39b94c3550a3f4f807b1325d
car1762 <na422558@gmail.com>
2025-07-07 15:40:32 +0500

    🔄 Updated DB config, bug fixes, and improvements

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const { connectToMongooDB } = require("./connect");
-const urlRoute = require("./routes/url");
+const { checkForAuthentication, restrictTo } = require("./middlewares/auth");
 const URL = require("./models/url");
-const staticRoute = require("./routes/staticRouter");
+
 const app = express();
 const PORT = 8001;
+
+//routes
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 connectToMongooDB("mongodb://localhost:27017/short-url").then(
   console.log("MongoDB connected")
@@ -16,6 +22,13 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthentication);
+
+//route using on the app
+app.use("/url", restrictTo(["NORMAL", "ADMIN"]), urlRoute);
+app.use("/", staticRoute);
+app.use("/user", userRoute);
 
 app.get("/test", async (req, res) => {
   const allUrls = await URL.find({});
@@ -23,10 +36,6 @@ app.get("/test", async (req, res) => {
     urls: allUrls,
   });
 });
-
-app.use("/url", urlRoute);
-
-app.use("/", staticRoute);
 
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,14 +1,28 @@
 const express = require("express");
-
+const { restrictTo } = require("../middlewares/auth");
 const router = express.Router();
-const URL = require("../models/url"); 
+const URL = require("../models/url");
 
-
-router.get(["/", ""], async (req, res) => {
-    const allurls = await URL.find({})
-    return res.render("home", {
+router.get("/admin/urls", restrictTo(["ADMIN"]), async (req, res) => {
+  const allurls = await URL.find({});
+  return res.render("home", {
     urls: allurls,
-    })
-})
+  });
+});
+
+router.get("/", restrictTo(["NORMAL", "ADMIN"]), async (req, res) => {
+  const allurls = await URL.find({ createdBy: req.user._id });
+  return res.render("home", {
+    urls: allurls,
+  });
+});
+
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
+
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
 
 module.exports = router;
